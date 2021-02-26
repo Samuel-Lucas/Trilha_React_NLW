f4a8cc5b91da4dc7dcd7a947d59b4a6eb324e140
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-02-26 16:23:18 -0300

    Aula 04

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Profile } from '../components/Profile'
 import styles from '../styles/pages/Home.module.css'
 
 import Head from 'next/head'
+import { CountdownProvider } from "../contexts/CountdownContext"
 
 
 export default function Home() {
@@ -18,17 +19,19 @@ export default function Home() {
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
 
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
